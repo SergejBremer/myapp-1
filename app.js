ab6a357666989c613f012a522e024da7455ac0ab
jankosacc <janis.koselevs@accenture.com>
2017-02-09 15:22:03 +0200

    Adding basic react

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-require('./less/style.less');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Picogram from './main';
+
+ReactDOM.render(<Picogram />,
+  document.getElementById('picogram'));
---
+++ main.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default MainPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// //import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
+//
+// import HomePage from './pages/HomePage';
+// // import ImagePage from './pages/RegisterPage';
+// // import UploadPage from './pages/RegisterPage';
+// // import ErrorPage from './pages/ErrorPage';
+//
+// require('font-awesome/css/font-awesome.css');
+// require('./less/style.less');
+//
+// class Picogram extends React.Component {
+//   render() {
+//     return (
+//       <Router history={hashHistory}>
+//         <Route path='/' component={LoginPage} />
+//         <Route path='/home' component={ErrorPage} />
+//         <Route path='/login' component={LoginPage} />
+//         <Route path='/register' component={RegisterPage} />
+//         <Route path='/forgot' component={ErrorPage} />
+//
+//         <Route path='/user' component={ErrorPage} />
+//
+//       </Router>
+//     );
+//   }
+// }
+//
+// export default Picogram;
